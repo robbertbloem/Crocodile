fc511759b2a61a0f8be1064a3d3fa6727d9b6ecc
Robbert <robbertbloem@gmail.com>
2013-02-11 22:32:07 +0100

    finished adding __future__ statements

--- Tests/script_c.py
+++ Tests/script_c.py
@@ -38,6 +38,8 @@ use 'all' to do (2) together
 
 from __future__ import print_function
 from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
 import os
 
