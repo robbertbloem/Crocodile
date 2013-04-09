58cb7fcec965bdc5ac0dcce5c5b3503afe13e9a9
Robbert <robbertbloem@gmail.com>
2013-04-09 09:18:42 +0200

    if FWHM of pulse length is above a limit, try again

--- Plugins/PulseLength.py
+++ Plugins/PulseLength.py
@@ -123,8 +123,17 @@ def print_res(A, FWHM, dt):
     print(u"%10s = %.1f fs (\u00B1 %.1f)" % ("FWHM", FWHM, dt))
 
 
+def default_A():
+    sigma = 50 # fs
+    mu = 0      
+    offset = 0
+    scale = 50
+    frequency = 0.055 # 1/fs
+    phase = 0
+    return [sigma, mu, offset, scale, frequency, phase]
 
-def calculate_pulse_length(data, A = [], dt = CONST.hene_fringe_fs, plot_results = True, print_results = True):
+
+def calculate_pulse_length(data, A = [], dt = CONST.hene_fringe_fs, plot_results = True, print_results = True, fwhm_limit = 200, mu_shift = 10, flag_recursive_limit = False):
 
     """
     Calculate the pulse length
@@ -135,6 +144,9 @@ def calculate_pulse_length(data, A = [], dt = CONST.hene_fringe_fs, plot_results
     dt (float, HeNe-fringe in fs): to convert from indices to time
     plot (BOOL, True): plot the results
     print_res (BOOL, True): print the results
+    fwhm_limit (float, 200): if FWHM is above this limit, it will try again by shifting the mean a bit (by mu_shift)
+    mu_shift (float, 10): shift the mean a bit
+    flag_recursive_limit (BOOL, False): function will try again if set to False. To prevent endless recursion.
     
     OUTPUT:
     - the output can be plotted to confirm the fit
@@ -147,13 +159,7 @@ def calculate_pulse_length(data, A = [], dt = CONST.hene_fringe_fs, plot_results
 
     # input for a single IR pulse    
     if A == []:
-        sigma = 50 # fs
-        mu = 0      
-        offset = 0
-        scale = 50
-        frequency = 0.055 # 1/fs
-        phase = 0
-        A = [sigma, mu, offset, scale, frequency, phase]
+        A = default_A()
     
     data /= numpy.amax(data)
     
@@ -176,21 +182,33 @@ def calculate_pulse_length(data, A = [], dt = CONST.hene_fringe_fs, plot_results
     A = [sigma, mu, offset, scale]
     y_env = envelope(A,t)
 
-    # use the envelope to calculate the FWHM
+    # for the FWHM we need a higher time resolution
+    t2 = numpy.arange(len(data)*10) - 5*len(data)
+    t2 *= dt / 10
+    y_fwhm = envelope(A,t2)
+     # use the envelope to calculate the FWHM
     # make it positive
-    if y_env[150] < 0:
-        y_env = -y_env
-    max_env = numpy.amax(y_env)
+    if y_fwhm[int(len(t2)/2)] < 0:
+        y_fwhm = -y_fwhm
+    max_fwhm = numpy.amax(y_fwhm)
     # select the part of the list
-    l = numpy.where(y_env > max_env/2)[0]
+    l = numpy.where(y_fwhm > max_fwhm/2)[0]
     # the length of the list in indices, multiply with time distance
-    FWHM = len(l) * dt
+    FWHM = len(l) * dt / 10
+ 
+    if FWHM > fwhm_limit and flag_recursive_limit == False:
+        # FWHM is above the limit, try again by shifting the mean a bit
+        print("Trying again...")
+        A = default_A()
+        A[1] = mu_shift
+        FWHM = calculate_pulse_length(data, A = A, dt = dt, plot_results = plot_results, print_results = print_results, flag_recursive_limit = True)
     
-    if plot_results:
-        plot_res(t, data, y_conv, y_single, y_env)
-    
-    if print_results:
-        print_res(A_out, FWHM, dt)
+    else:
+        # yay! a reasonable result. plot and print the results.
+        if plot_results:
+            plot_res(t, data, y_conv, y_single, y_env)
+        if print_results:
+            print_res(A_out, FWHM, dt)
     
     return FWHM
 
