a708e95e4ea3df4cbce0a43070121fbda099a4b8
Robbert <robbertbloem@gmail.com>
2015-11-05 09:59:35 +0100

    Started with import_show_spectrum

--- Mosquito_methods.py
+++ Mosquito_methods.py
@@ -28,9 +28,9 @@ class show_shots(DCC.dataclass):
         self.verbose("New pe_col class", flag_verbose)
         DCC.dataclass.__init__(self, objectname = objectname, flag_verbose = flag_verbose)
 
-
-    def set_file_info(self, data_folder, date, basename, timestamp):
-        self.set_file_dict(data_folder, date, basename, timestamp)    
+# 
+#     def set_file_info(self, data_folder, date, basename, timestamp):
+#         self.set_file_dict(data_folder, date, basename, timestamp)    
 
 
     def import_data(self):
@@ -83,9 +83,6 @@ class show_shots(DCC.dataclass):
         self.b_axes = [w3_axis, sh, self.ds, self.sp, sc]
         self.b_units = ["w3 (cm-1)", "Shots", "Datastates", "Spectra", "Scans"]
         
-#         self.b_n = [n_w3, n_sh, n_ds, n_sp, n_sc]
-
-
         self.b_choppers = numpy.zeros([3, n_sh, n_sc])
         self.b_specials = numpy.zeros([15, n_sh, n_sc])
 
@@ -119,6 +116,111 @@ class show_shots(DCC.dataclass):
             
         plt.show()
 
+
+
+
+class show_spectrum(DCC.dataclass):
+    """
+    pe_col: implementation class for colinear 2D-IR setup. This includes all importing functions.
+    """
+
+    def __init__(self, objectname, flag_verbose = False):
+        self.verbose("New pe_col class", flag_verbose)
+        DCC.dataclass.__init__(self, objectname = objectname, flag_verbose = flag_verbose)
+
+
+    def import_data(self):
+        """
+        This method splits up file importing for supporting files and measurement files. It first finds the file format file. It will use this to test if file_dict is correct. 
+        """
+        # check if _file_dict is set
+        if self._file_dict["base_folder"] == "" or self._file_dict["base_filename"] == "":
+            self.printError("No file information set.", inspect.stack()) 
+            return False   
+        
+        # find LV_file_format, also a check if _file_dict is correct
+        try: 
+            self.file_format = IOM.find_LV_fileformat(
+                base_folder = self._file_dict["base_folder"], 
+                flag_verbose = self.flag_verbose
+            )   
+            if self.file_format == -1:  
+                self.printError("File_format file found, but was not able to parse it.", inspect.stack()) 
+                return False  
+        except FileNotFoundError:
+            self.printError("File_format file not found.", inspect.stack()) 
+            return False 
+
+        w3_axis, n_w3 = IOM.import_wavenumbers(self._file_dict, self.file_format, flag_verbose = self.flag_verbose)
+
+#         n_sh = IOM.import_nshots(self._file_dict, self.file_format, flag_verbose = self.flag_verbose)
+#         sh = numpy.arange(n_sh)
+        
+        n_sh = 1
+        sh = numpy.array([0])
+        
+        n_ds = IOM.find_number_of_datastates(self._file_dict["base_folder"], flag_verbose = self.flag_verbose)
+        n_ds = int(n_ds/2)
+        
+        n_sp = IOM.import_nspectra(self._file_dict, self.file_format, flag_verbose = self.flag_verbose)
+
+        spds, dump, dump = IOM.import_spectraAndDatastates(self._file_dict, self.file_format, flag_verbose = self.flag_verbose)
+        
+        self.sp = spds[:,0]
+        self.ds = spds[:,1]
+        for i in range(n_ds):
+            if self.ds[i] == "-1":
+                self.ds[i] = 1
+            else:
+                self.ds[i] = 0
+        self.ds = numpy.array(self.ds, dtype = "int")
+
+        n_sc = IOM.find_number_of_scans(self._file_dict["base_folder"], self._file_dict["base_filename"], self._file_dict["extension"], flag_verbose = self.flag_verbose)
+        sc = numpy.arange(n_sc)
+        
+        self.b_n = [n_w3, n_sh, 2*n_ds, n_sp, n_sc]
+        self.b = numpy.empty(self.b_n)
+        self.b_noise = numpy.empty(self.b_n)
+        self.b_axes = [w3_axis, sh, self.ds, self.sp, sc]
+        self.b_units = ["w3 (cm-1)", "Shots", "Datastates", "Spectra", "Scans"]
+        
+        self.s_n = numpy.copy(self.b_n)
+        self.s_n[2] = 1
+        self.s = numpy.zeros(self.s_n)
+        self.s_noise = numpy.zeros(self.s_n)
+
+        sh = 0        
+        for sp in range(self.b_n[3]): 
+            for sc in range(self.b_n[4]): 
+                
+                ds = 0
+                suffix = "sp%i_ds%i_signal_%i" % (sp, ds, sc)
+                self.s[:,sh,ds,sp,sc] = IOM.import_file(self._file_dict, suffix, self.flag_verbose) 
+                suffix = "sp%i_ds%i_signal_noise_%i" % (sp, ds, sc)
+                self.s_noise[:,sh,ds,sp,sc] = IOM.import_file(self._file_dict, suffix, self.flag_verbose)
+              
+                for ds in range(self.b_n[2]): 
+                    suffix = "sp%i_ds%i_intensity_%i" % (sp, ds, sc)
+                    self.b[:,sh,ds,sp,sc] = IOM.import_file(self._file_dict, suffix, self.flag_verbose) 
+                    suffix = "sp%i_ds%i_intensity_noise_%i" % (sp, ds, sc)
+                    self.b_noise[:,sh,ds,sp,sc] = IOM.import_file(self._file_dict, suffix, self.flag_verbose)
+
+
+
+    def make_plot(self):  
+
+        axes = numpy.arange(self.b_n[1])
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111)    
+        
+        for ds in range(self.b_n[2]):
+            for sc in range(self.b_n[4]):
+                data = self.b[15,:,ds,0,sc]
+                mask = numpy.isfinite(data)            
+                ax.plot(axes[mask], data[mask], marker = ".", linestyle = "none")
+            
+        plt.show()
 
 
 
--- Tests/Mosquite_methods_tests.py
+++ Tests/Mosquite_methods_tests.py
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 import Crocodile.Mosquito_methods as MM
 import PythonTools.Debug as DEBUG
 
+plt.close("all")
+
 # init argument parser
 parser = argparse.ArgumentParser(description='Command line arguments')
 
@@ -22,14 +24,14 @@ imp.reload(MM)
 global suite_list
 suite_list = [
     "Suite 1: Show shots",
-#     "Suite 2: Test of importing",
+    "Suite 2: Show spectrum",
 ]
 
 # add arguments
 parser.add_argument("-v", "--verbose", action = "store_true", help = "Increase output verbosity")
 parser.add_argument("-r", "--reload", action = "store_true", help = "Reload modules")
 parser.add_argument("-s1", "--skip1", action = "store_true", help = suite_list[0])
-# parser.add_argument("-s2", "--skip2", action = "store_true", help = suite_list[1])
+parser.add_argument("-s2", "--skip2", action = "store_true", help = suite_list[1])
 # parser.add_argument("-s3", "--skip3", action = "store_true", help = suite_list[2])
 # parser.add_argument("-s4", "--skip4", action = "store_true", help = suite_list[3])
 
@@ -50,13 +52,73 @@ def execute(args):
     else:
         DEBUG.verbose("Skipping: " + suite_list[0], True)
         
-#     if args.skip2 == False:
-#         suite = unittest.TestLoader().loadTestsFromTestCase(Test_of_importing)
-#         unittest.TextTestRunner(verbosity=1).run(suite) 
-#     else:
-#         DEBUG.verbose("Skipping: " + suite_list[1], True)
+    if args.skip2 == False:
+        suite = unittest.TestLoader().loadTestsFromTestCase(Test_show_spectrum)
+        unittest.TextTestRunner(verbosity=1).run(suite) 
+    else:
+        DEBUG.verbose("Skipping: " + suite_list[1], True)
 
+class Test_show_spectrum(unittest.TestCase):
+    """
 
+    CHANGELOG:
+    20151104/RB: started the suite
+
+    """
+    #############
+    ### SETUP ###
+    #############
+    def setUp(self):
+        self.flag_verbose = args.verbose  
+
+        
+    def test_f1(self):
+
+        file_dict_A = {
+            "data_folder": "/Users/robbert/Developer/Crocodile/Tests/Test_resources",
+            "date": "20010101",
+            "basename": "show_spectrum_f1",
+            "timestamp": "141014",
+            "extension": ".csv",
+        }  
+        objectname = "test A"   
+        A = MM.show_spectrum(objectname, flag_verbose = self.flag_verbose)
+        A.set_file_dict(file_dict_A["data_folder"], file_dict_A["date"], file_dict_A["basename"], file_dict_A["timestamp"])
+        A.import_data()
+#         A.make_plot()
+        print(A.file_format)
+
+    def test_pp(self):
+
+        file_dict_A = {
+            "data_folder": "/Users/robbert/Developer/Crocodile/Tests/Test_resources",
+            "date": "20010101",
+            "basename": "show_spectrum_pp",
+            "timestamp": "143628",
+            "extension": ".csv",
+        }  
+        objectname = "test A"   
+        A = MM.show_spectrum(objectname, flag_verbose = self.flag_verbose)
+        A.set_file_dict(file_dict_A["data_folder"], file_dict_A["date"], file_dict_A["basename"], file_dict_A["timestamp"])
+        A.import_data()
+#         A.make_plot()
+        print(A.file_format)
+
+    def test_pp_sepds(self):
+
+        file_dict_A = {
+            "data_folder": "/Users/robbert/Developer/Crocodile/Tests/Test_resources",
+            "date": "20010101",
+            "basename": "show_spectrum_pp_sepds",
+            "timestamp": "145025",
+            "extension": ".csv",
+        }  
+        objectname = "test A"   
+        A = MM.show_spectrum(objectname, flag_verbose = self.flag_verbose)
+        A.set_file_dict(file_dict_A["data_folder"], file_dict_A["date"], file_dict_A["basename"], file_dict_A["timestamp"])
+        A.import_data()
+#         A.make_plot()
+        print(A.file_format)    
 
 class Test_show_shots(unittest.TestCase):
     """
